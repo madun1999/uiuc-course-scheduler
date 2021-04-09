a1f7b4ef4bf49670626c86d65b55031044f40dab
Dun Ma <madun1999@gmail.com>
2021-04-09 01:47:49 -0500

    initialize react app

---
+++ course-scheduler/types.tsx
@@ -0,0 +1,20 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  TabOne: undefined;
+  TabTwo: undefined;
+  Login: undefined;
+  Profile: undefined;
+};
+
+export type TabOneParamList = {
+  TabOneScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
+
